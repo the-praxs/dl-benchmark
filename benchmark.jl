e79347ca2e7c9dfbdd3c49bd2c1b26c5e6aa3499
Pratyush Shukla <ps4534@nyu.edu>
2022-12-19 19:52:17 -0500

    Added benchmarking code

--- benchmark.jl
+++ benchmark.jl
@@ -4,11 +4,11 @@ using Flux.Optimise: Optimiser, WeightDecay
 using Flux: onehotbatch, onecold
 using Flux.Losses: logitcrossentropy
 using Statistics, Random
-using Logging: with_logger
-using TensorBoardLogger: TBLogger, tb_overwrite, set_step!, set_step_increment!
-using ProgressMeter: @showprogress
 import MLDatasets
-import BSON
+import CUDA
+
+include("models/lenet.jl")
+using .lenet: LeNet
 
 function prepare_data(args)
     xtrain, ytrain = MLDatasets.MNIST.traindata(Float32)
@@ -27,7 +27,7 @@ end
 
 loss(ŷ, y) = logitcrossentropy(ŷ, y)
 
-function eval_loss_accuracy(loader, model, device)
+function eval_model(loader, model, device)
     l = 0f0
     acc = 0
     ntot = 0
@@ -47,16 +47,16 @@ round4(x) = round(x, digits=4)
 
 # arguments for the `train` function 
 Base.@kwdef mutable struct Args
-    η = 3e-4             # learning rate
-    λ = 1e-6                # L2 regularizer param, implemented as weight decay
-    batchsize = 32      # batch size
-    epochs = 10          # number of epochs
-    seed = 42             # set seed > 0 for reproducibility
-    use_cuda = false      # if true use cuda (if available)
-    infotime = 1 	     # report every `infotime` epochs
-    checktime = 5        # Save the model every `checktime` epochs. Set to 0 for no checkpoints.
-    tblogger = true      # log training with tensorboard
-    savepath = "runs/"    # results path
+    η = 0.1                 # learning rate
+    λ = 5e-4                # L2 regularizer param, implemented as weight decay
+    batchsize = 128         # batch size
+    epochs = 10             # number of epochs
+    seed = 42               # set seed > 0 for reproducibility
+    use_cuda = true         # if true use cuda (if available)
+    infotime = 1 	        # report every `infotime` epochs
+    checktime = 5           # Save the model every `checktime` epochs. Set to 0 for no checkpoints.
+    tblogger = false        # log training with tensorboard
+    savepath = "runs/"      # results path
 end
 
 function train(; kws...)
@@ -66,73 +66,57 @@ function train(; kws...)
     
     if use_cuda
         device = gpu
-        @info "Training on GPU"
+        @info "Training on GPU..."
     else
         device = cpu
-        @info "Training on CPU"
+        @info "Training on CPU..."
     end
 
     ## DATA
-    train_loader, test_loader = prepare_data(args)
+    @info "Data Loading Time..."
+    @time train_loader, test_loader = prepare_data(args)
     @info "Dataset MNIST: $(train_loader.nobs) train and $(test_loader.nobs) test examples"
 
     ## MODEL AND OPTIMIZER
-    model = LeNet5() |> device
-    @info "LeNet5 model: $(num_params(model)) trainable params"    
+    model = LeNet() |> device
+    @info "LeNet model: $(num_params(model)) trainable params"    
     
     ps = Flux.params(model)  
 
-    #opt = ADAM(args.η) 
-    opt = AdaBelief(args.η)
+    # opt = ADAM(args.η) 
+    opt = Descent(args.η)
     if args.λ > 0 # add weight decay, equivalent to L2 regularization
         opt = Optimiser(WeightDecay(args.λ), opt)
     end
     
     ## LOGGING UTILITIES
-    if args.tblogger 
-        tblogger = TBLogger(args.savepath, tb_overwrite)
-        set_step_increment!(tblogger, 0) # 0 auto increment since we manually set_step!
-        @info "TensorBoard logging at \"$(args.savepath)\""
-    end
-    
     function report(epoch)
-        train = eval_loss_accuracy(train_loader, model, device)
-        test = eval_loss_accuracy(test_loader, model, device)        
+        train = eval_model(train_loader, model, device)
+        test = eval_model(test_loader, model, device)        
         println("Epoch: $epoch   Train: $(train)   Test: $(test)")
-        if args.tblogger
-            set_step!(tblogger, epoch)
-            with_logger(tblogger) do
-                @info "train" loss=train.loss  acc=train.acc
-                @info "test"  loss=test.loss   acc=test.acc
-            end
-        end
     end
     
     ## TRAINING
-    @info "Start Training"
+    @info "Evaluating model..."
     report(0)
-    for epoch in 1:args.epochs
-        @showprogress for (x, y) in train_loader
-            x, y = x |> device, y |> device
-            gs = Flux.gradient(ps) do
-                    ŷ = model(x)
-                    loss(ŷ, y)
+    @time begin
+        for epoch in 1:args.epochs
+            @time begin
+                for (x, y) in train_loader
+                    x, y = x |> device, y |> device
+                    gs = Flux.gradient(ps) do
+                            ŷ = model(x)
+                            loss(ŷ, y)
+                        end
+                
+                    Flux.Optimise.update!(opt, ps, gs)
                 end
-
-            Flux.Optimise.update!(opt, ps, gs)
-        end
-        
-        ## Printing and logging
-        epoch % args.infotime == 0 && report(epoch)
-        if args.checktime > 0 && epoch % args.checktime == 0
-            !ispath(args.savepath) && mkpath(args.savepath)
-            modelpath = joinpath(args.savepath, "model.bson") 
-            let model = cpu(model) #return model to cpu before serialization
-                BSON.@save modelpath model epoch
             end
-            @info "Model saved in \"$(modelpath)\""
+        
+            ## Printing and logging
+            epoch % args.infotime == 0 && report(epoch)
         end
     end
 end
 
-train()
+@time train()
